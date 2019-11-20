8e9cffa7507cb7729f59b6651d5828a01157a09e
kashima-dot-com <kiruvtuber@gmail.com>
2019-11-20 13:34:23 -0500

    Add promises and update examples

--- README.md
+++ README.md
@@ -6,7 +6,12 @@ The first(?) API wrapper for sketchel.
 ```js
 const sketchel = require('sketchel.js')
 const avatar = require('fs').createWriteStream('./avatar.png')
-sketchel.getAvatar('nekomikoreimu').pipe(avatar)
+sketchel.getAvatar('nekomikoreimu').then(body => body.pipe(avatar))
+```
+### Get user
+```js
+const sketchel = require('./index')
+sketchel.getUser('nekomikoreimu').then(body => console.log(body))
 ```
 
 # How to install?
--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const fetch = require('node-fetch')
+const BASE_URL = 'https://sketchel.art/api/v1'
 
 module.exports = {
-  getAvatar: (id = 'nekomikoreimu') => {
-    fetch(`https://sketchel.art/api/v1/get-avatar/${id}`)
-      .then(res => {
-        if (res.status === 404) return new Error('Userid doesn\'t exist.')
-        return res.body
-      })
+  getAvatar: (id = false) => {
+    return new Promise((resolve, reject) => {
+      if (!id) reject(new Error('Didn\'t provide ID.'))
+
+      fetch(`${BASE_URL}/get-avatar/${id}`)
+        .then(res => {
+          if (res.status === 404) reject(new Error('Userid doesn\'t exist.'))
+          resolve(res.body)
+        })
+    })
+  },
+  getUser: (id = false) => {
+    return new Promise((resolve, reject) => {
+      if (!id) reject(new Error('Didn\'t provide ID.'))
+
+      fetch(`${BASE_URL}/get-user/${id}`)
+        .then(res => {
+          if (res.status === 404) reject(new Error('Userid doesn\'t exist.'))
+          res.json().then(body => resolve(body))
+        })
+    })
   }
 }
